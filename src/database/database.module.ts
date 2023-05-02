ca206ed2a9964de905e78f86ee1a981fd2ffd739
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-05-02 13:22:30 +0200

    feat: database setup

---
+++ src/database/database.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ENV } from 'src/common/constant';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_USERNAME: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+        MONGO_DATABASE: Joi.string().required(),
+        MONGO_HOST: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get(ENV.MONGO_USERNAME);
+        const password = configService.get(ENV.MONGO_PASSWORD);
+        const database = configService.get(ENV.MONGO_DATABASE);
+        const host = configService.get(ENV.MONGO_HOST);
+
+        return {
+          uri: `mongodb+srv://${username}:${password}@${host}/?authMechanism=DEFAULT`,
+          dbName: database,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class DatabaseModule {}
