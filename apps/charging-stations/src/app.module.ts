adaccef634295f65999cf8fca98c21b212d2dad2
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-05-04 08:36:13 +0200

    chore: added graphql federation driver

--- apps/charging-stations/src/app.module.ts
+++ apps/charging-stations/src/app.module.ts
@@ -2,21 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { StationsModule } from './stations/stations.module';
 import { DatabaseModule } from './database/database.module';
-import { ChargersModule } from './chargers/chargers.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
     }),
     StationsModule,
     DatabaseModule,
-    ChargersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
