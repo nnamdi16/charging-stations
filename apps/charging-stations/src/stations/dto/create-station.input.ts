a1a7fa05761e1e22d67a43db7d7313715e679c26
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-05-04 08:39:34 +0200

    chore: refactored the input type using GraphQLJson object

--- apps/charging-stations/src/stations/dto/create-station.input.ts
+++ apps/charging-stations/src/stations/dto/create-station.input.ts
@@ -1,7 +1,26 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
+import { Exclude } from 'class-transformer';
+import { StatusType } from '../entities/models/statusType.model';
+import { GraphQLJSONObject } from 'graphql-type-json';
+import { OperatorInfo } from '../entities/models/operatorInfo.model';
+import { AddressInfo } from '../entities/models/addressInfo.model';
+import { Connections } from '../entities/models/connections.model';
 
 @InputType()
 export class CreateStationInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @Exclude()
+  _id: string;
+
+  @Field((type) => GraphQLJSONObject)
+  operatorInfo: OperatorInfo;
+
+  @Field((type) => GraphQLJSONObject)
+  statusType: StatusType;
+
+  @Field((type) => GraphQLJSONObject)
+  addressInfo: AddressInfo;
+
+  @Field((type) => [GraphQLJSONObject])
+  connections: [Connections];
 }
